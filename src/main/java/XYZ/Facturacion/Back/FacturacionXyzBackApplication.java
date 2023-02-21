655ba89ea6199019c11b0970cb85ec7047cafff2
Fabian <fabiam0226@gmail.com>
2023-02-21 11:57:00 -0500

    Primer commit

---
+++ src/main/java/XYZ/Facturacion/Back/FacturacionXyzBackApplication.java
@@ -0,0 +1,13 @@
+package XYZ.Facturacion.Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionXyzBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionXyzBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/XYZ/Facturacion/Back/FacturacionXyzBackApplicationTests.java
@@ -0,0 +1,13 @@
+package XYZ.Facturacion.Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionXyzBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
